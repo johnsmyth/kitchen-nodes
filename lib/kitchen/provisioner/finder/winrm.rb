d39a56144fc885a11eccb9deb7ff2de8164e3a31
John Smyth <john.smyth@wowinc.com>
2016-03-05 12:49:58 -0600

    added ipconfig logic to finder/winrm.rb

--- lib/kitchen/provisioner/finder/winrm.rb
+++ lib/kitchen/provisioner/finder/winrm.rb
@@ -22,12 +22,34 @@ module Kitchen
         end
 
         def find_ips
+          powershell_version = @connection.node_execute(
+            '$PSVersionTable.psversion.major').stdout.chomp
+          if powershell_version.to_i > 2
+            find_ips_via_get_netipconfiguration
+          else
+            find_ips_via_ipconfig
+          end
+        end
+
+        def find_ips_via_get_netipconfiguration
           out = @connection.node_execute(
             'Get-NetIPConfiguration | % { $_.ipv4address.IPAddress }')
           data = []
           out[:data].each do |out_data|
             stdout = out_data[:stdout]
             data << stdout.chomp unless stdout.nil? || stdout.chomp.empty?
+          end
+          data
+        end
+
+        def find_ips_via_ipconfig
+          out = @connection.node_execute(
+            '(ipconfig) -match \'IPv4 Address\'')
+          data = []
+          out[:data].each do |out_data|
+            stdout = out_data[:stdout]
+            # data << $1 if stdout.chomp =~ /:\s*(\S+)/
+            data << Regexp.last_match[1] if stdout.chomp =~ /:\s*(\S+)/
           end
           data
         end
